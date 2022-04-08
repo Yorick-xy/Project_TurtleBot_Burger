d5f57306c42b31fd5714dfdea0f54a0ae9e20066
Carolan <100842960+CarolanC@users.noreply.github.com>
2022-04-08 19:55:11 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
-#TODO lancement de la navigation
+from setuptools import setup
+
+package_name = 'turtlebot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='caro',
+    maintainer_email='caro@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'follow = turtlebot.publisher_member_function:main',
+        ],
+    },
+)
